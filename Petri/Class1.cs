b7dd0ca1fdf5e1c5e92f1e8b62d78a6ae00da17d
Karell Ste-Marie <karell.stemarie@gmail.com>
2012-10-31 16:56:10 -0400

    Initial Commit

---
+++ Petri/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petri
+{
+    public class Place : Base
+    {
+        public List<Token> Tokens { get; private set; }
+
+        public Place()
+        {
+            Tokens = new List<Token>();
+        }
+    }
+
+    public class Transition : Base
+    { }
+
+    public class Token
+    {
+        public Place Place { get; private set; }
+    }
+
+    public class Arc
+    {
+        public Base Source { get; private set; }
+        public Base Destination { get; private set; }
+        public int Capacity { get; private set; }
+    }
+
+    public class Base
+    {
+        public List<Arc> Arcs { get; private set; }
+    }
+}
